ac669fcc5b6fb8eb4c6afdc36e20091559053712
Mariano ~Kiwi <marianovitale2394@gmail.com>
2020-08-30 02:34:34 -0300

    make github page

---
+++ docs/precache-manifest.99bf4e6ac5601106bbd43babe2f49c4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7c890f497a62b1977ae7a39c51faa74",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ede14be5b5e3cbda1420",
+    "url": "/static/css/main.6b7b0221.chunk.css"
+  },
+  {
+    "revision": "0abab1f8bd7ea1cca163",
+    "url": "/static/js/2.5f090955.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5f090955.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ede14be5b5e3cbda1420",
+    "url": "/static/js/main.dbdecdb0.chunk.js"
+  },
+  {
+    "revision": "e5ee6826b74226f47af5",
+    "url": "/static/js/runtime-main.b577d66e.js"
+  }
+]);
